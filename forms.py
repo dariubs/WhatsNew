7eb99655096346c07f5842154cd6922fe9d882c7
dariush abbasi <dariushem@yahoo.com>
2015-05-13 18:45:54 +0430

    create routes

---
+++ forms.py
@@ -0,0 +1,12 @@
+from flask.ext.wtf import Form
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(Form):
+	username = StringField("username")
+	password = StringField("password")
+
+class RegisterForm(Form):
+	username = StringField("username")
+	password = StringField("password")  
--- routes.py
+++ routes.py
@@ -4,23 +4,72 @@
 	WhatsNew
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, abort, session
+from forms import LoginForm, RegisterForm
+from flask import request
+
 
 app = Flask(__name__)
 
+app.secret_key = "guess me"
 
+# Home
 @app.route("/")
 def home():
-	return ":)"
+	if 'username' in session :
+		return "Hello"
+	else :
+		return redirect(url_for('login'))
 
-@app.route("/login")
+# Register
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = RegisterForm()
+
+	if request.method == 'POST':
+		return "Hi"
+
+	return render_template("register.html", form=form)
+
+
+# Login
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-	return render_template("login.html")
+	form = LoginForm()
 
+	if request.method == 'POST':
+		# Dummy Login
+		if request.form["username"] == "admin" and request.form["password"] == "admin" :
+			session["username"] = request.form["username"]
+			return redirect(url_for('home'))
+	return render_template("login.html", form=form)
+
+# Logout
+@app.route("/logout")
+def logout() :
+	if 'username' in session :
+		session.pop('username', None)
+		return redirect(url_for('home'))
+	else :
+		return redirect(url_for('login'))
+
+# Submit Posts
 @app.route("/submit/new")
 def submit_new():
 	return render_template("post.html")
 
 
+# User Page
+@app.route("/user/<username>")
+def userpage(username):
+	return "Hello, %s" % username
+
+# 404
+@app.errorhandler(404)
+def not_found() :
+	render_template("404.html"), 404
+
+
 if __name__ == '__main__':
+	app.debug = False
 	app.run()
