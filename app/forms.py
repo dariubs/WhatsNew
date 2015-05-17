bc71c8198bf8f7119975bb75eb683f5fe0f58dbd
dariush abbasi <dariushem@yahoo.com>
2015-05-17 19:38:44 +0430

    change static & templates directory path

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,6 @@ from flask.ext.wtf import Form
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
-
 class LoginForm(Form):
 	username = StringField("username")
 	password = StringField("password")
