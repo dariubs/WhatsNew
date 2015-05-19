2c5bb83f372797d34d599ec9eb306c0f073c72c4
dariush abbasi <dariushem@yahoo.com>
2015-05-19 18:17:09 +0430

    edit models & some config

--- app/config.py
+++ app/config.py
@@ -8,3 +8,11 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
 app.secret_key = "guess me"
+
+DB = {
+	"name" : "dbname",
+	"host" : "0.0.0.0",
+	"port" : 1234,
+	"username" : "root",
+	"password" : "password"
+}
--- app/models.py
+++ app/models.py
@@ -1,26 +1,34 @@
 from peewee import *
 import datetime
+from app.config import *
+from flask.ext.login import UserMixin
+from flask.ext.bcrypt import generate_password_hash
 
-DATABASE = SqliteDatabase('news.db')
+DATABASE = peewee.MySQLDatabase(DB_NAME, host=DB_HOST, port=DB_PORT, user=DB_USERNAME, passwd=DB_PASSWORD)
 
-class User(Model):
+class BaseModel(UserMixin, Model):
+	class Meta :
+		database = DATABASE
+
+class User(BaseModel):
 	username = CharField(unique=True)
 	email = CharField(unique=True)
 	password = CharField()
 	realname = CharField()
-	website = CharField()
-	joindate = DateTimeField(datetime.datetime.now)
-
-	class Meta :
-		database = DATABASE
+	join_date = DateTimeField(datetime.datetime.now)
 
 
-class Link(Model):
-	linkurl = TextField()
-	linktitle = TextField()
-	author = IntegerField()
-	createtime = DateTimeField(datetime.datetime.now)
+class Link(BaseModel):
+	link_id = IntegerField()
+	link_url = TextField()
+	link_title = TextField()
+	author = ForeignKeyField(User, related_name="pets")
+	pub_date = DateTimeField(datetime.datetime.now)
 	upvote = IntegerField()
 
-	class Meta :
-		database = DATABASE
+class Comment(BaseModel):
+	comment_id = IntegerField()
+	comment = TextField()
+	link = ForeignKeyField(Link, related_name="linkid")
+	parent_comment = IntegerField()
+	comment_date = DateTimeField(datetime.datetime.now)
