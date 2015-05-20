a0b9abd67978e82d2173102c41f25cf6a0cec3c5
dariush abbasi <dariushem@yahoo.com>
2015-05-21 01:24:25 +0430

    add models methods prototype

--- app/models.py
+++ app/models.py
@@ -14,9 +14,20 @@ class User(BaseModel):
 	username = CharField(unique=True)
 	email = CharField(unique=True)
 	password = CharField()
-	realname = CharField()
 	join_date = DateTimeField(datetime.datetime.now)
+	is_active = BooleanField()
 
+	@classmethod
+	def create_user(user, username, email, password):
+		pass
+
+	@classmethod
+	def activate(user, activecode):
+		pass
+
+	@classmethod
+	def user_info(user, id):
+		pass
 
 class Link(BaseModel):
 	link_id = IntegerField()
@@ -26,12 +37,47 @@ class Link(BaseModel):
 	pub_date = DateTimeField(datetime.datetime.now)
 	upvote = IntegerField()
 
+	@classmethod
+	def add_link(link, url, title, author):
+		pass
+
+	@classmethod
+	def rm_link(link, id):
+		pass
+
+	@classmethod
+	def up_vote(link, user):
+		pass
+
+	@classmethod
+	def hotlinks(link, page, limit):
+		pass
+
+	@classmethod
+	def newlinks(link, page, limit):
+		pass
+
+
+
 class Comment(BaseModel):
 	comment_id = IntegerField()
 	comment = TextField()
 	link = ForeignKeyField(Link, related_name="linkid")
 	parent_comment = IntegerField()
 	comment_date = DateTimeField(datetime.datetime.now)
+
+	@classmethod
+	def add_comment(cmnt, comment, link, parent):
+		pass
+
+	@classmethod
+	def rm_comment(cmnt, id):
+		pass
+
+	@classmethod
+	def comments(cmnt, link, page, limit):
+		pass
+
 
 def create_schemas():
 	db.connect()
